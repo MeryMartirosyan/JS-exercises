b9132999eaa553697f101c66f1eb02e3891e10ba
Mery Martirosyan <mmeri5359@gmail.com>
2024-09-15 21:32:38 -0700

    exercise.js

--- exercise.js
+++ exercise.js
@@ -1,1 +1,11 @@
-
+// if vowels are in string, count = count + 1
+function vowelCounted(str) {
+    let count = 0;
+    let arr = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    for(let i = 0; i < str.length; i++) {
+       if(arr.includes(str[i])) {
+           count++;
+       }
+    }
+    return count
+}
